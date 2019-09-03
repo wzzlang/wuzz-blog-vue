1a46c7c8ecf8f9a6b4d44afe7445d6579f277af0
wzzlang2@163.com <wzzlang2@>
2019-09-03 13:01:39 +0800

    新建项目

---
+++ src/components/bottom/bottom.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="bottom">
+      <Footer style="background: black;color: white;" >
+    <p>
+      Copyright © wzzlang 博客 All Rights Reserved 闽ICP备17002346号
+    </p>
+      </Footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "bottom"
+    }
+</script>
+
+<style scoped>
+  #bottom p{
+   display:inline-block;
+   width:100%;
+   height:auto;
+ }
+</style>
